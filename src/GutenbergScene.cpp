1134b020389a1cc2153b8e49c60c101f2c0b98da
杜奇蔚 <2051500@tongji.edu.cn>
2022-11-17 20:06:37 +0800

    change ground obj

--- src/GutenbergScene.cpp
+++ src/GutenbergScene.cpp
@@ -7,7 +7,7 @@ GutenbergScene::GutenbergScene()
 
     // objects
 	//ground = new MovingObject("ground", 0, multilightShader->getShader(), btVector3(0.01, 0.01, 0.01), btVector3(0, 0, 0));
-    concav_ground = new StaticObject("concav_ground", multilightShader->getShader(), btVector3(0.07, 0.07, 0.07), btVector3(-10.6, 0, 3.8));
+    concav_ground = new StaticObject("concav_ground", multilightShader->getShader(), btVector3(0.3, 0.3, 0.3), btVector3(0, 0, 0));
 	neck_walker = new MovingObject("neck_walker", 1.0, multilightShader->getShader(), btVector3(0.1, 0.1, 0.1), btVector3(0, 10, 0));
 }
 
